a142ab87e778d116d5fd871177e9f67bb3a5eac4
Tomo Matsushita <tomo.mats@gmail.com>
2016-11-10 20:26:33 -0500

    full house

---
+++ lib/to-poker/full_house.rb
@@ -0,0 +1,22 @@
+module ToPoker
+  class FullHouse
+    def self.run(hand)
+      counter = {}
+
+      hand.cards.each do |card|
+        if counter[card.rank]
+          counter[card.rank] += 1
+        else
+          counter[card.rank] = 1
+        end
+      end
+
+      p counter.values
+      if counter.values == [3,2] || counter.values == [2,3]
+        return true
+      else
+        return false
+      end
+    end
+  end
+end
---
+++ spec/unit/to-poker/full_house_spec.rb
@@ -0,0 +1,25 @@
+module ToPoker
+  RSpec.describe FullHouse do
+    it 'should evaluate a full house' do
+      result = FullHouse.run(Hand.new([
+        Card.new(:five, :hearts),
+        Card.new(:five, :diamonds),
+        Card.new(:five, :spades),
+        Card.new(:three, :hearts),
+        Card.new(:three, :diamonds)
+      ]))
+      expect(result).to eq(true)
+    end
+
+    it 'should not evaluate a full house' do
+      result = FullHouse.run(Hand.new([
+        Card.new(:two, :hearts),
+        Card.new(:five, :diamonds),
+        Card.new(:five, :spades),
+        Card.new(:three, :hearts),
+        Card.new(:three, :diamonds)
+      ]))
+      expect(result).to eq(false)
+    end
+  end
+end
